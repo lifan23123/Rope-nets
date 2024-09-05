df59b652cc867672827a94078c25a0b61ebba78f
lifan <1641172976@qq.com>
2024-09-05 18:25:51 +0800

    项目初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import pinia from "./store";
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+app.use(pinia);
+app.mount("#app");
---
+++ src/view/layout/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="layout-nav">nav</div>
+  <div class="layout-aside">aside</div>
+  <div class="layout-content">content</div>
+  <div class="layout-footer">footer</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.layout-nav {
+  position: fixed;
+  background-color: aqua;
+}
+.layout-aside {
+  background-color: brown;
+}
+.layout-content {
+  margin-left: 200px;
+  background-color: yellow;
+}
+.layout-footer {
+  position: fixed;
+  background-color: orange;
+}
+</style>
